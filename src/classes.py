3eda99c2bf900f06d31673101bb7f8c48046f6eb
Василий <kulikov821999@gmail.com>
2025-11-02 23:12:18 +0300

    added condition for products if param is none or there is no param 'products'

--- src/classes.py
+++ src/classes.py
@@ -34,10 +34,10 @@ class Category:
     category_count: int = 0
     product_count: int = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products
+        self.products = products if products is not None else []
         Category.category_count += 1
         self.product_count = len(self.products)
 
